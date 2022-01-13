658da19f43a3afd4b5b01ababb928329f937c189
unknown <ageevdmitry2018@gmail.com>
2022-01-13 10:39:06 +0300

    добавляет отображение статистики с помощью генерации checkbox

--- src/view/statistic-board-view.js
+++ src/view/statistic-board-view.js
@@ -1,40 +1,45 @@
-import dayjs from 'dayjs';
-import isBetween from 'dayjs/plugin/isBetween';
 import Smart from './smart.js';
 import {Chart} from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import {FilterType} from '../const.js';
+import {FilterType, StatisticCheckbox} from '../const.js';
 import {filter} from '../utils/filter.js';
 import {getChartData, getTopGenre, getChartDuration, isBetweenDate} from '../utils/films.js';
-dayjs.extend(isBetween);
 
-const createStatisticBoardViewTemplate = (userRank, watchedCount, totalDuration, topGenre) => (
+const createStatisticBoardViewTemplate = (userRank, watchedCount, totalDuration, topGenre, statisticPeriod) => {
 
-  `<section class="statistic">
+  const getStatisticCheckboxTemplate = (check) => {
+
+    const {type, name} = check;
+
+    return (
+      `<input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-${type}" value="${type}" ${type === statisticPeriod ? 'checked' : ''}>
+      <label for="statistic-${type}" class="statistic__filters-label">${name}</label>`
+    );
+  };
+
+  const getStatisticFormTemplate = () => {
+
+    const statisticCheckboxTemplate = StatisticCheckbox.map((check) => getStatisticCheckboxTemplate(check)).join('');
+
+    return (
+      `<form action="https:cho.htmlacademy.ru/" method="get" class="statistic__filters">
+      <p class="statistic__filters-description">Show stats:</p>
+      ${statisticCheckboxTemplate}
+      </form>`
+    );
+  };
+
+  const statisticFormTemplate = getStatisticFormTemplate();
+
+  return (
+
+    `<section class="statistic">
     <p class="statistic__rank">
       Your rank
       <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
       <span class="statistic__rank-label">${userRank}</span>
     </p>
-
-    <form action="https:cho.htmlacademy.ru/" method="get" class="statistic__filters">
-      <p class="statistic__filters-description">Show stats:</p>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
-      <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-      <label for="statistic-today" class="statistic__filters-label">Today</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-      <label for="statistic-week" class="statistic__filters-label">Week</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-      <label for="statistic-month" class="statistic__filters-label">Month</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-      <label for="statistic-year" class="statistic__filters-label">Year</label>
-    </form>
+    ${statisticFormTemplate}
 
     <ul class="statistic__text-list">
       <li class="statistic__text-item">
@@ -59,7 +64,8 @@ const createStatisticBoardViewTemplate = (userRank, watchedCount, totalDuration,
     </div>
 
   </section>`
-);
+  );
+};
 
 const createChartElement = (statisticCtx, data) => {
   const BAR_HEIGHT = 50;
@@ -149,17 +155,15 @@ export default class StatisticBoardView extends Smart {
   }
 
   getTemplate() {
-    return createStatisticBoardViewTemplate(this._userRank, this._wathedFilmsCount, this._totalDuration, this._topGenre);
+    return createStatisticBoardViewTemplate(this._userRank, this._wathedFilmsCount, this._totalDuration, this._topGenre, this._statisticPeriod);
   }
 
   _choiceStatisticPeriod(evt) {
     evt.preventDefault();
     this._statisticPeriod = evt.target.value;
     this._getData();
-    // this.updateElement();
     this.updateData(this._filterFilms);
     this._setCharts();
-    console.log(evt.target.value);
   }
 
   setStatisticPeriodClickHandlers() {
@@ -189,3 +193,4 @@ export default class StatisticBoardView extends Smart {
     this._totalDuration = getChartDuration(this._filterFilms);
   }
 }
+
